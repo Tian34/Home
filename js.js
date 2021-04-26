d69064c7f591b796fb0ee4fcb105971565e762b3
Tian34 <70531781+Tian34@users.noreply.github.com>
2021-04-25 21:13:21 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,32 @@
+var candado=function(){
+
+		var n1=document.getElementById("p1");
+		var n2=document.getElementById("p11");
+		
+
+		n2.className="c1";}
+
+var candado1=function(){
+
+		var n1=document.getElementById("p2");
+		var n2=document.getElementById("p22");
+		
+
+		n2.className="c2";}
+
+var candado2=function(){
+
+		var n1=document.getElementById("p3");
+		var n2=document.getElementById("p33");
+		
+
+		n2.className="c3";}
+
+var candado3=function(){
+
+		var n1=document.getElementById("p4");
+		var n2=document.getElementById("p44");
+		
+
+		n2.className="c4";}
+		
